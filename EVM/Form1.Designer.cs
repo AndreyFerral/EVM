baf61302fd225fec7db021055c99c5042cf42849
Andrey Yakovlev <tigra9922@gmail.com>
2021-04-23 19:06:27 +1000

    Добавьте файлы проекта.

---
+++ EVM/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace EVM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Roboto Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(6, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Линейная";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Roboto Condensed", 9F);
+            this.textBox1.Location = new System.Drawing.Point(44, 159);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Roboto Condensed", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 14;
+            this.listBox1.Location = new System.Drawing.Point(3, 250);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 116);
+            this.listBox1.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(264, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Red;
+            series1.LabelBorderWidth = 3;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.YValuesPerPoint = 2;
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Color = System.Drawing.Color.Blue;
+            series2.LabelBorderWidth = 3;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series2.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(700, 500);
+            this.chart1.TabIndex = 48;
+            this.chart1.Text = "chart1";
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Roboto Condensed", 9F);
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 14;
+            this.listBox2.Location = new System.Drawing.Point(129, 250);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 116);
+            this.listBox2.TabIndex = 49;
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Roboto Condensed", 9F);
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 14;
+            this.listBox3.Location = new System.Drawing.Point(3, 396);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 116);
+            this.listBox3.TabIndex = 50;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Roboto Condensed", 9F);
+            this.textBox2.Location = new System.Drawing.Point(44, 187);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 22);
+            this.textBox2.TabIndex = 51;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Roboto Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(6, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 27);
+            this.button2.TabIndex = 52;
+            this.button2.Text = "Степенная";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Roboto Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(6, 100);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 27);
+            this.button3.TabIndex = 53;
+            this.button3.Text = "Экспоненциальная";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(246, 139);
+            this.groupBox1.TabIndex = 54;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выберите аппроксимацию";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 19);
+            this.label1.TabIndex = 55;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(20, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 19);
+            this.label2.TabIndex = 56;
+            this.label2.Text = "b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(8, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 19);
+            this.label3.TabIndex = 57;
+            this.label3.Text = "Значения x:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(129, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 19);
+            this.label4.TabIndex = 58;
+            this.label4.Text = "Значения y:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(8, 374);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 19);
+            this.label5.TabIndex = 59;
+            this.label5.Text = "Значения Fi:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 528);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Построение графиков аппроксимации";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ EVM/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BotAgent.DataExporter;
+
+namespace EVM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            double SumElementsX = 0;
+            double Sum2ElementsX = 0;
+
+            double SumElementsY = 0;
+            double Sum2ElementsY = 0;
+
+            double SumMultiplyElementsXY = 0;
+
+            Excel xl = new Excel();
+            xl.FileOpen("ЭВМ_Вариант10.xlsx");
+
+            int CountXY = 91;
+            string[] X = new string[CountXY];
+            string[] Y = new string[CountXY];
+            double[] Fi = new double[CountXY];
+
+            for (int count = 1; count < CountXY; count++) { 
+                X[count] = xl.Rows[count][0];
+                Y[count] = xl.Rows[count][1];
+
+                SumElementsX += Convert.ToDouble(X[count]);
+                SumElementsY += Convert.ToDouble(Y[count]);
+                Sum2ElementsX += Math.Pow(Convert.ToDouble(X[count]), 2);
+                Sum2ElementsY += Math.Pow(Convert.ToDouble(Y[count]), 2);
+                SumMultiplyElementsXY += Convert.ToDouble(X[count]) * Convert.ToDouble(Y[count]);
+
+                listBox1.Items.Add(X[count]);
+                listBox2.Items.Add(Y[count]);
+
+                chart1.Series[0].Points.AddXY(Convert.ToDouble(X[count]), Convert.ToDouble(Y[count]));
+            }
+
+            double a = (Sum2ElementsX * SumElementsY - SumElementsX * SumMultiplyElementsXY) / ((CountXY - 1) * Sum2ElementsX - Math.Pow(SumElementsX, 2));
+            double b = ((CountXY - 1) * SumMultiplyElementsXY - SumElementsX * SumElementsY) / ((CountXY - 1) * Sum2ElementsX - Math.Pow(SumElementsX, 2));
+            textBox1.Text = Convert.ToString(a);
+            textBox2.Text = Convert.ToString(b);
+
+            for (int count = 1; count < CountXY; count++)
+            {
+                Fi[count] = a + b * Convert.ToDouble(X[count]);
+                listBox3.Items.Add(Fi[count]);
+                chart1.Series[1].Points.AddXY(Convert.ToDouble(X[count]), Convert.ToDouble(Fi[count]));
+
+            }
+
+            /*
+            textBox1.Text = Convert.ToString(SumMultiplyElementsXY);
+            textBox2.Text = Convert.ToString(SumMultiplyElementsXY);
+            */
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            double SumElementsX = 0;
+            double Sum2ElementsX = 0;
+
+            double SumElementsY = 0;
+            double Sum2ElementsY = 0;
+
+            double SumMultiplyElementsXY = 0;
+
+            Excel xl = new Excel();
+            xl.FileOpen("ЭВМ_Вариант10.xlsx");
+
+            int CountXY = 91;
+            string[] X = new string[CountXY];
+            string[] Y = new string[CountXY];
+            double[] Fi = new double[CountXY];
+            double[] lnX = new double[CountXY];
+            double[] lnY = new double[CountXY];
+
+            for (int count = 1; count < CountXY; count++)
+            {
+                X[count] = xl.Rows[count][0];
+                Y[count] = xl.Rows[count][1];
+
+                lnX[count] = Math.Log(Convert.ToDouble(X[count]));
+                lnY[count] = Math.Log(Convert.ToDouble(Y[count]));
+
+                SumElementsX += Convert.ToDouble(lnX[count]);
+                SumElementsY += Convert.ToDouble(lnY[count]);
+                Sum2ElementsX += Math.Pow(Convert.ToDouble(lnX[count]), 2);
+                Sum2ElementsY += Math.Pow(Convert.ToDouble(lnY[count]), 2);
+                SumMultiplyElementsXY += Convert.ToDouble(lnX[count]) * Convert.ToDouble(lnY[count]);
+
+                listBox1.Items.Add(X[count]);
+                listBox2.Items.Add(Y[count]);
+
+                chart1.Series[0].Points.AddXY(Convert.ToDouble(X[count]), Convert.ToDouble(Y[count]));
+            }
+
+            double a = (Sum2ElementsX * SumElementsY - SumElementsX * SumMultiplyElementsXY) / ((CountXY - 1) * Sum2ElementsX - Math.Pow(SumElementsX, 2));
+            double b = ((CountXY - 1) * SumMultiplyElementsXY - SumElementsX * SumElementsY) / ((CountXY - 1) * Sum2ElementsX - Math.Pow(SumElementsX, 2));
+            a = Math.Exp(a);
+            textBox1.Text = Convert.ToString(a);
+            textBox2.Text = Convert.ToString(b);
+
+            for (int count = 1; count < CountXY; count++)
+            {
+                Fi[count] = a * Math.Pow(Convert.ToDouble(X[count]), b);
+                listBox3.Items.Add(Fi[count]);
+                chart1.Series[1].Points.AddXY(Convert.ToDouble(X[count]), Convert.ToDouble(Fi[count]));
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            double SumElementsX = 0;
+            double Sum2ElementsX = 0;
+
+            double SumElementsY = 0;
+            double Sum2ElementsY = 0;
+
+            double SumMultiplyElementsXY = 0;
+
+            Excel xl = new Excel();
+            xl.FileOpen("ЭВМ_Вариант10.xlsx");
+
+            int CountXY = 91;
+            string[] X = new string[CountXY];
+            string[] Y = new string[CountXY];
+            double[] Fi = new double[CountXY];
+            double[] lnX = new double[CountXY];
+            double[] lnY = new double[CountXY];
+
+            for (int count = 1; count < CountXY; count++)
+            {
+                X[count] = xl.Rows[count][0];
+                Y[count] = xl.Rows[count][1];
+
+                lnX[count] = Math.Log(Convert.ToDouble(X[count]));
+                lnY[count] = Math.Log(Convert.ToDouble(Y[count]));
+
+                SumElementsX += Convert.ToDouble(X[count]);
+                SumElementsY += Convert.ToDouble(lnY[count]);
+                Sum2ElementsX += Math.Pow(Convert.ToDouble(X[count]), 2);
+                Sum2ElementsY += Math.Pow(Convert.ToDouble(lnY[count]), 2);
+                SumMultiplyElementsXY += Convert.ToDouble(X[count]) * Convert.ToDouble(lnY[count]);
+
+                listBox1.Items.Add(X[count]);
+                listBox2.Items.Add(Y[count]);
+
+                chart1.Series[0].Points.AddXY(Convert.ToDouble(X[count]), Convert.ToDouble(Y[count]));
+            }
+
+            double a = (Sum2ElementsX * SumElementsY - SumElementsX * SumMultiplyElementsXY) / ((CountXY - 1) * Sum2ElementsX - Math.Pow(SumElementsX, 2));
+            double b = ((CountXY - 1) * SumMultiplyElementsXY - SumElementsX * SumElementsY) / ((CountXY - 1) * Sum2ElementsX - Math.Pow(SumElementsX, 2));
+            a = Math.Exp(a);
+            textBox1.Text = Convert.ToString(a);
+            textBox2.Text = Convert.ToString(b);
+
+            for (int count = 1; count < CountXY; count++)
+            {
+                Fi[count] = a * Math.Exp(Convert.ToDouble(X[count]) * b);
+                listBox3.Items.Add(Fi[count]);
+                chart1.Series[1].Points.AddXY(Convert.ToDouble(X[count]), Convert.ToDouble(Fi[count]));
+
+            }
+        }
+    }
+}
